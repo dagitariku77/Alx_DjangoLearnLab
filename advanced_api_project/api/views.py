a14ecffb4bf670a480b09722354fc0a4471d57a9
dagitariku77 <dtariku80@gmail.com>
2025-03-23 21:11:31 +0300

    Add files via upload

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+# advanced_api_project/api/views.py
+from rest_framework import generics, permissions
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Allow read for all, write only for authenticated
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Allow read for all, write only for authenticated
+
+class AuthorList(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Allow read for all
+
+class AuthorDetail(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Allow read for all
